ed7266123823f1df9d9ad66c3c2861606cd18045
Happy-Franck <solofonirina35@gmail.com>
2026-05-21 11:02:11 +0300

    fix(deep-links): serve /.well-known/assetlinks.json with application/json Content-Type

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/.well-known/assetlinks.json', function () {
+    return response()->file(public_path('.well-known/assetlinks.json'), [
+        'Content-Type' => 'application/json',
+    ]);
+});
